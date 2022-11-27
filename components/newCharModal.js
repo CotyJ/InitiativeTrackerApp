b5f8d3aadf1b5d3a3221b431291cec6b19912d61
coty <Coty.Janeway@gmail.com>
2022-11-26 18:42:28 -0700

    Started on New Character Modal

---
+++ components/newCharModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import appStyles from '../appStyles';
+
+const newCharModal = () => {
+  return (
+    <View>
+      <Modal>
+        <Text>I'm a modal!</Text>
+      </Modal>
+    </View>
+  );
+};
+
+export default newCharModal;
